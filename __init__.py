8f1f80e83c500ab420874896e0f97226409bbb42
jarbasal <jarbasai@mailfence.com>
2018-05-02 19:35:53 +0100

    modernize, use padatious

--- __init__.py
+++ __init__.py
@@ -15,31 +15,20 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
-from os.path import dirname, join
-
-from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
+from mycroft import MycroftSkill, intent_file_handler
 
 
 # TODO - Localization
 
 class SpeakSkill(MycroftSkill):
-    @intent_handler(IntentBuilder("").require("Speak").require("Words"))
+    @intent_file_handler("speak.intent")
     def speak_back(self, message):
         """
             Repeat the utterance back to the user.
 
-            TODO: The method is very english centric and will need
-                  localization.
         """
-        # Remove everything up to the speak keyword and repeat that
-        utterance = message.data.get('utterance')
-        repeat = re.sub('^.*?' + message.data['Speak'], '', utterance)
+        repeat = message.data.get("sentence", "")
         self.speak(repeat.strip())
-
-    def stop(self):
-        pass
 
 
 def create_skill():
